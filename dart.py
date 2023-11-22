d90156c4eea84207a89efa0afc4a74ec95b60840
Lihan Shen <leoshen216@gmail.com>
2023-11-22 20:19:05 +0000

    hough circle detection added
    hough circle detection added

--- dart.py
+++ dart.py
@@ -3,6 +3,7 @@ import cv2
 import os
 import sys
 import argparse
+import math
 
 parser = argparse.ArgumentParser(description='dart detection')
 parser.add_argument('-name', '-n', type=str, default='Dartboard/dart0.jpg')
@@ -91,7 +92,123 @@ def click_event(event, x, y, flags, param):
         print(f'Coordinates: ({x}, {y})')
 
 
+def sobel_x(input):
+    sobelxOutput = np.zeros((input.shape[0], input.shape[1]), dtype=np.float32)
+    kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    kernelRadiusX = 1
+    kernelRadiusY = 1
+    paddedInput = cv2.copyMakeBorder(input, kernelRadiusX, kernelRadiusX, kernelRadiusY, kernelRadiusY,
+                                     cv2.BORDER_REPLICATE)
+
+    for i in range(0, input.shape[0]):
+        for j in range(0, input.shape[1]):
+            patch = paddedInput[i:i + 3, j:j + 3]
+            sum = (np.multiply(patch, kernel)).sum()
+            sobelxOutput[i, j] = sum
+
+    return sobelxOutput
+
+
+def sobel_y(input):
+    sobelyOutput = np.zeros((input.shape[0], input.shape[1]), dtype=np.float32)
+    kernel = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+    kernelRadiusX = 1
+    kernelRadiusY = 1
+    paddedInput = cv2.copyMakeBorder(input, kernelRadiusX, kernelRadiusX, kernelRadiusY, kernelRadiusY,
+                                     cv2.BORDER_REPLICATE)
+
+    for i in range(0, input.shape[0]):
+        for j in range(0, input.shape[1]):
+            patch = paddedInput[i:i + 3, j:j + 3]
+            sum = (np.multiply(patch, kernel)).sum()
+            sobelyOutput[i, j] = sum
+
+    return sobelyOutput
+
+
+def gradient_mag(input):
+    x = sobel_x(input)  # Assuming sobel_x(input) correctly computes the x-direction gradient
+    y = sobel_y(input)  # Assuming sobel_y(input) correctly computes the y-direction gradient
+    result = np.sqrt(x ** 2 + y ** 2)  # Calculate the magnitude of the gradient
+    return result
+
+
+def gradient_ori(input):
+    x = sobel_x(input)  # Assuming sobel_x(input) correctly computes the x-direction gradient
+    y = sobel_y(input)
+    result = np.arctan2(y, x + math.exp(-10))
+    # Normalize the result to the range [0, 255]
+    # normalized_result = ((result - result.min()) / (result.max() - result.min())) * 255
+    # Convert the result to uint8 data type for visualization
+    # result_float32 = normalized_result.astype(np.float32)
+    # change_to_pi= (result/180)*math.pi
+    return result
+
+
+def thresholdPixel(input, ts):
+    for i in range(0, input.shape[0]):
+        for j in range(0, input.shape[1]):
+            if input[i][j] > ts:
+                input[i][j] = 255
+            else:
+                input[i][j] = 0
+    return input
+
+
+def hough_circle_detection(gradient_magnitude, gradient_orientation, ts, th, min_radius, max_radius):
+    # Step 1: Threshold the gradient magnitude image.
+    thresholded_image = thresholdPixel(gradient_magnitude, ts)
+
+    # Step 2: Create a 3D Hough Space array.
+    hough_space = np.zeros((gradient_magnitude.shape[0], gradient_magnitude.shape[1], max_radius - min_radius + 1),
+                           dtype=np.uint32)
+
+    # Step 3: Voting in Hough Space.
+    for y in range(gradient_magnitude.shape[0]):
+        for x in range(gradient_magnitude.shape[1]):
+            if thresholded_image[y, x] != 0:
+                for radius in range(min_radius, max_radius + 1):
+                    center_x = int(x + radius * np.cos(gradient_orientation[y, x]))
+                    center_y = int(y + radius * np.sin(gradient_orientation[y, x]))
+
+                    if 0 <= center_x < gradient_magnitude.shape[1] - radius and 0 <= center_y < \
+                            gradient_magnitude.shape[0] - radius:
+                        hough_space[center_y, center_x, radius - min_radius] += 1
+
+                    center_x0 = int(x - radius * np.cos(gradient_orientation[y, x]))
+                    center_y0 = int(y - radius * np.sin(gradient_orientation[y, x]))
+                    if 0 <= radius <= center_x0 < gradient_magnitude.shape[1] - radius and 0 <= radius <= center_y0 < \
+                            gradient_magnitude.shape[0] - radius:
+                        hough_space[center_y0, center_x0, radius - min_radius] += 1
+    # Step 4: Thresholding the Hough Space.
+    thresholded_hough = np.zeros((hough_space.shape[0], hough_space.shape[1], hough_space.shape[2]), dtype=np.uint32)
+    for y in range(hough_space.shape[0]):
+        for x in range(hough_space.shape[1]):
+            for z in range(hough_space.shape[2]):
+                if hough_space[y, x, z] > th:
+                    thresholded_hough[y, x, z] = 1
+
+    # Step 5: Find circle centers.
+    centers = np.argwhere(thresholded_hough == 1)
+    detected_circles = []
+    for center in centers:
+        detected_circles.append((center[0], center[1], center[2] + min_radius))
+
+    # Step 6: Display the Hough Space.
+    hough_space_sum = np.sum(hough_space, axis=2)
+
+    # Step 7: Overlay detected circles on the original image.
+    # for circle in detected_circles:
+    #     cv2.circle(frame, (circle[1], circle[0]), circle[2], (255, 0, 0), 2)
+
+    return thresholded_image, hough_space_sum, detected_circles
+
+
+
+
+
 # ==== MAIN ==============================================
+
 for i in range(0, 16):
     imageName = f'Dartboard/dart{i}.jpg'
     imageNameEdit = imageName.split('/')[-1].split('.')[0]
@@ -121,7 +238,6 @@ for i in range(0, 16):
     iou_threshold = 0.5  # Set threshold value
     ground_truth_boxes = []  # Replace with your list of ground truth bounding boxes
 
-
     # Extract the detected boxes for the current image
     print('Image :' + imageNameEdit)
     detected_boxes = detectAndDisplay(frame)
@@ -146,7 +262,7 @@ for i in range(0, 16):
         if best_box is not None:
             best_matches[m] = best_box
 
-    # Now, best_matches contains each ground truth box matched with the best detected box
+    # best_matches contains each ground truth box matched with the best detected box
     for gt_index, box in best_matches.items():
         print(f"Ground truth box {ground_truth_boxes[gt_index]} best matched with detected box {box} with IOU: "
               f"{calculate_iou(box, ground_truth_boxes[gt_index])}")
@@ -161,12 +277,55 @@ for i in range(0, 16):
     precision = true_positives / (true_positives + false_positives + 0.1)
     recall = TPR  # Recall is the same as TPR
 
+    # F1 score
     F1_score = 2 * (precision * recall) / ((precision + recall) + 0.1)
     print(f"F1 score: {F1_score}")
     print("\n")
-    # Save the image
-    filename = f"Result_Image/result{i}.jpg"  # This creates the filename string
-    cv2.imwrite(filename, frame)  # This saves the image
+
+    resultName = f"Result_Image/result{i}.jpg"
+    filenameForThreshold = f"Result_Image/thresholded_image{i}.jpg"
+
+    #cv2.imwrite(resultName, frame)
+
+
+
+
+
+    # Hough Circle Detection##############################################
+    # Read Input Image from source
+    frame_circle = cv2.imread(imageName, 1)
+
+    if not (type(frame_circle) is np.ndarray):
+        print('Not image data')
+        sys.exit(1)
+
+    gray_image = cv2.cvtColor(frame_circle, cv2.COLOR_BGR2GRAY)
+    gray_image = gray_image.astype(np.float32)
+    gray_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    gradient_magnitude = gradient_mag(gray_image)
+    gradient_orientation = gradient_ori(gray_image)
+    ts = 150
+    th = 15
+    min_radius = 40
+    max_radius = 240
+
+    # cv2.imwrite("grayimage.jpg", gray_image)
+
+    # cv2.imwrite("gradient_magnitude.jpg", gradient_magnitude)
+    # cv2.imwrite("gradient_orientation.jpg", gradient_orientation)
+    thresholded_image, hough_space_sum, detectedCircles = hough_circle_detection(gradient_magnitude, gradient_orientation, ts, th,
+                                                                     min_radius, max_radius)
+
+    for circle in detectedCircles:
+      cv2.circle(frame, (circle[1], circle[0]), circle[2], (255, 0, 0), 2)
+    cv2.imwrite(filenameForThreshold, thresholded_image)
+    # hough_space_sum = hough_space_sum.astype(np.uint8)
+    # cv2.imwrite("hough_space_sum.jpg", hough_space_sum)
+
+    cv2.imwrite(resultName, frame)
+
+
+    # This saves the image
     # cv2.namedWindow('image')  # Name the window
     # # cv2.setMouseCallback('image', click_event)
     # cv2.imshow('image', frame)
